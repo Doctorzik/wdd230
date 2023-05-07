422a9733da8f8d2b551a24ddb72d40c68d5e508e
doctorzik <ezekieljeremiah92@gmail.com>
2023-05-07 02:53:15 +0100

    done

---
+++ lesson3/scripts/design-plan.js
@@ -0,0 +1,12 @@
+ 
+
+
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let currentDate =  `${year}`
+document.getElementById("year").textContent = currentDate;
+document.getElementById("currentDate").textContent = document.lastModified;
