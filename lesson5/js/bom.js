db325e4f9c1047832ef9c0ac2d1a80280777fd42
doctorzik <ezekieljeremiah92@gmail.com>
2023-05-20 20:33:01 +0100

    sas

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -2,18 +2,30 @@
 const button = document.querySelector("button")
 const input = document.querySelector("#favchap")
 const list = document.querySelector("#list")
+console.log(button)
+console.log(input)
+console.log(list)
 
-button.addEventListener("click", function(){
-     const listItem = document.createElement("li");
+
+button.addEventListener("click", function() {
+  var mes =    input.value;
+
+     const li = document.createElement("li");
+          console.log(li)
+     
      const del = document.createElement("button");
-     listItem.textContent = input;
+
+        li.textContent= input.value;
      del.textContent = "&#10060";
-     const my = listItem.append(del)
-
-     list.append(my);
+    var me =  li.append(del)
 
 
-     del.addEventListener("click", del.remove(listItem))
+     list.append(li, del);
+
+
+
+
+     del.addEventListener("click", li.remove())
 
      input.focus();
 
