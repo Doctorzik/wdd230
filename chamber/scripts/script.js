6a9581803a65ca86ab7ca201a4454a69004f26ce
doctorzik <ezekieljeremiah92@gmail.com>
2023-05-25 13:48:40 +0100

    gg

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -7,9 +7,12 @@ let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
+
 let currentDate =  `${year}`
 document.getElementById("year").textContent = currentDate;
 document.getElementById("currentDate").textContent = document.lastModified;
+
+
 
 
 function toggleMenu(){
@@ -22,3 +25,10 @@ function toggleMenu(){
  const x = document.getElementById("button")
  
  x.onclick = toggleMenu;
+
+ let today = date.getDay();
+ 
+
+if (today === 1 || today === 2 ){
+  document.querySelector(".banner").style.display = "unset"
+}
