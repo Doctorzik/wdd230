407b872026258758f0b58ec682e2ed286e5011ff
doctorzik <ezekieljeremiah92@gmail.com>
2023-06-04 00:10:43 +0100

    sa

---
+++ chamber/scripts/localstore.js
@@ -0,0 +1,15 @@
+let date = new Date();
+const millPerDay = 1000 * 60 * 60 * 24;
+
+
+localStorage.setItem("visitDate", date.getTime());
+
+const lastVisit = localStorage.getItem("visitDate");
+
+const newDate = date.getTime();
+const oldDate = lastVisit;
+const millDiff = newDate - oldDate;
+
+const numberOfDays = millDiff / millPerDay;
+
+document.querySelector("#local").textContent = numberOfDays;
--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -32,21 +32,3 @@ function toggleMenu(){
 if (today === 1 || today === 2 ){
   document.querySelector(".banner").style.display = "unset"
 }
-
-// This is the script for the local storage.
-
-
-const millPerDay = 1000 * 60 * 60 * 24;
-
-
-localStorage.setItem("visitDate", date.getTime());
-
-const lastVisit = localStorage.getItem("visitDate");
-
-const newDate = date.getTime();
-const oldDate = lastVisit;
-const millDiff = newDate - oldDate;
-
-const numberOfDays = millDiff / millPerDay;
-
-document.querySelector("#local").textContent = numberOfDays;
