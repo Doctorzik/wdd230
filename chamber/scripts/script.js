e77886bbaece63229322fbd7cf5ed90ad0a0e63d
doctorzik <ezekieljeremiah92@gmail.com>
2023-06-04 00:01:18 +0100

    kk

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -35,18 +35,18 @@ if (today === 1 || today === 2 ){
 
 // This is the script for the local storage.
 
-let getNow = new Date();
+
 const millPerDay = 1000 * 60 * 60 * 24;
 
 
-localStorage.setItem("visitDate", getNow.getTime());
+localStorage.setItem("visitDate", date.getTime());
 
 const lastVisit = localStorage.getItem("visitDate");
 
-const newDate = getNow.getTime();
+const newDate = date.getTime();
 const oldDate = lastVisit;
 const millDiff = newDate - oldDate;
 
-const numDays = millDiff / millPerDay;
+const numberOfDays = millDiff / millPerDay;
 
-document.querySelector("#local").textContent = numDays;
+document.querySelector("#local").textContent = numberOfDays;
