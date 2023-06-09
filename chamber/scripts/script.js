3b667ce816e53d95070963fa2659a889e68a94a6
doctorzik <ezekieljeremiah92@gmail.com>
2023-06-09 21:57:00 +0100

    llll

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -8,12 +8,16 @@ let month = date.getMonth() + 1;
 let year = date.getFullYear();
 
 
+
+
 let currentDate =  `${year}`
 document.getElementById("year").textContent = currentDate;
 document.getElementById("currentDate").textContent = document.lastModified;
 let  timestampField = document.getElementById("timestamp");
 timestampField.value = date
 
+
+document.querySelector(".tt").textContent= `${day} - ${month}- ${year}`
 
 
 
