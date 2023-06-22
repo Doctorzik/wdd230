696771ffd73afecc61dde5074f6d6881b44c164c
doctorzik <ezekieljeremiah92@gmail.com>
2023-06-23 00:39:56 +0100

    uu

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,17 +1,54 @@
-let windSpeed = document.getElementById("windspeed").textContent;
+let windSpeed = document.getElementById("windspeed");
 let windChill = document.getElementById("windchill");
-let temperature = document.getElementById("temperature").textContent;
+let temperature = document.getElementById("temperature");
+let weatherIcon = document.getElementById("weather-icon");
+let captionDesc = document.querySelector("figcaption");
 
-if ((temperature <= 10) & (windSpeed > 4.9)) {
+const url =
+  "http://api.openweathermap.org/data/2.5/weather?q=Bayelsa State&units=metric&limit=5&appid=59e29ea14e74049eec5fe8a71be46861";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      // this is for testing the call
+      displayResults(data);
+      console.log(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(weatherData) {
+  temperature.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
+  const imagesrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+  desc.charAt(0).toUpperCase();
+  weatherIcon.setAttribute("src", imagesrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.innerHTML = desc;
+  windSpeed.textContent = weatherData.wind.speed;
+}
+apiFetch();
+
+if ((temperature <= 10) & (windSpeed > 4.8)) {
   windChillFactor(temperature, windSpeed);
 } else {
   windChill.innerHTML = "N/A";
 }
 
+function windChillFactor(t, s) {
+  let windChillFactor =
+    35.74 +
+    0.6215 * t -
+    35.75 * Math.pow(s, 0.16) +
+    0.4275 * t * Math.pow(s, 0.16);
 
-function windChillFactor(t,s) { 
-let windChillFactor = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
-  
-  windChill.innerHTML= windChillFactor.toFixed(0)
+  windChill.innerHTML = windChillFactor.toFixed(0);
 }
-
